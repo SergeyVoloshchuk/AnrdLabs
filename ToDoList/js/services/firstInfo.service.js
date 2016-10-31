3d64a756a86c206d4bb23b0fc11b4b1fe0553d3b
SergeyVoloshchuk <sergeyvolochuc@gmail.com>
2016-10-31 17:23:43 +0300

    create service

---
+++ ToDoList/js/services/firstInfo.service.js
@@ -0,0 +1,30 @@
+(function () {
+    'use strict';
+    angular.module('app.helpers')
+        .service('firstInfoService', FirstInfoService);
+    FirstInfoService.$inject = ['$http', '$q']
+
+    function FirstInfoService($http, $q) {
+        var service = {
+            getData: getData,
+            getDataAsync: getDataAsync
+        };
+        return service;
+
+        function getData() {
+            return $http.get('js/json/first.json').then(function (res) {
+                return res.data;
+            })
+        }
+
+        function getDataAsync() {
+            var deferred = $q.defer();
+            setTimeout(function () {
+                console.log('first promise resolved');
+                deferred.resolve('1');
+            }, 2000);
+            return deferred.promise;
+        }
+
+    }
+})();
